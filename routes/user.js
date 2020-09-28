50280251ac8f3579f1073292e6b7029e8d7c1d00
dave <davejjceriola@gmail.com>
2020-09-28 20:24:00 +0800

    backend reg

--- routes/user.js
+++ routes/user.js
@@ -1,34 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../auth');
-const UserController = require('../controllers/user');
-router.get('/', (req, res) => {
-  res.send('you are querying from the users routes');
+const auth = require("../auth");
+const UserController = require("../controllers/user");
+router.get("/", (req, res) => {
+  res.send("you are querying from the users routes");
 });
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   UserController.handleRegistration(req.body).then((data) => {
+    console.log(data);
     res.send(data);
   });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   UserController.handleLogin(req.body).then((data) => {
     res.send(data);
   });
 });
 
-router.get('/userdetails', auth.verify, (req, res) => {
+router.get("/userdetails", auth.verify, (req, res) => {
   const user = auth.decode(req.headers.authorization);
   UserController.getUserDetails(user).then((data) => {
     res.status(200).json({
-      message: 'details successfully gathered',
+      message: "details successfully gathered",
       data: data,
     });
   });
 });
 
-router.post('/transaction', auth.verify, (req, res) => {
+router.post("/transaction", auth.verify, (req, res) => {
   const data = {
     id: auth.decode(req.headers.authorization).id,
     transaction: req.body,
@@ -39,13 +40,13 @@ router.post('/transaction', auth.verify, (req, res) => {
   });
 });
 
-router.put('/:transactionId', auth.verify, (req, res) => {
+router.put("/:transactionId", auth.verify, (req, res) => {
   const data = {
     userId: auth.decode(req.headers.authorization).id,
     transactionId: req.params.transactionId,
     transactionUpdate: {
-      type: 'Income',
-      description: 'testing updates',
+      type: "Income",
+      description: "testing updates",
       amount: 1000,
     },
   };
@@ -54,7 +55,7 @@ router.put('/:transactionId', auth.verify, (req, res) => {
     res.send(result);
   });
 });
-router.delete('/:transactionId', auth.verify, (req, res) => {
+router.delete("/:transactionId", auth.verify, (req, res) => {
   const data = {
     userId: auth.decode(req.headers.authorization).id,
     transactionId: req.params.transactionId,
